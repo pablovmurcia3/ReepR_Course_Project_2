199b904e8366d28478862b11d933e1b52cfc6884
Pablo Velásquez <pablovmurcia@yahoo.com>
2021-02-02 23:44:43 -0500

    sd a

--- preliminary script.R
+++ preliminary script.R
@@ -4,37 +4,65 @@
 fileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 download.file(fileUrl,destfile="Storm Data.bz2",method="curl") # it takes some minutes depending on your internet connection
 
-# Some transformation 
-# date
-data <- read.csv("Storm Data.bz2")
-str(data)
-data$BGN_DATE[1]
-data$BGN_DATE[902297]
+########### Decompression ###########
+install.packages("R.utils")
+library(R.utils)
+bunzip2("Storm Data.bz2", destname = "Storm DataDes", remove = FALSE)
+data <- read.csv("Storm DataDES")
 
-f <- grepl("0:00:00", data$BGN_DATE)
+#Select relevant variables
+library(dplyr)
+
+SelectedData <- data %>% select(REFNUM,STATE__,BGN_DATE,EVTYPE,LENGTH,WIDTH,MAG,
+                                 FATALITIES,INJURIES,PROPDMG,PROPDMGEXP,CROPDMG, 
+                                 CROPDMGEXP,WFO)
+
+######## Some transformation #############
+# date
+SelectedData$BGN_DATE[1]
+SelectedData$BGN_DATE[902297]
+
+f <- grepl("0:00:00", SelectedData$BGN_DATE)
 length(f)
 
 splitNames <- strsplit(data$BGN_DATE,"\\ ")
 firstElement <- function(x){x[1]}
+
 library(lubridate)
-data$BGN_DATE <- mdy(sapply(splitNames,firstElement))
-class(data$BGN_DATE)
-
-hist(data$BGN_DATE, breaks = 10)
+SelectedData$BGN_DATE <- year(mdy(sapply(splitNames,firstElement)))
 
 
-# Events (EVTYpe)
-names(data)
-data$EVTYPE
-unique(data$EVTYPE)
+
+
+
+
+##### Events (EVTYpe) ############
+names(SelectedData)
+unique(SelectedData$EVTYPE)
 frecevents<-sort(table(data$EVTYPE))
 
-barplot(frecevents[975:985], cex.names = 0.5)
+
+table(filter(SelectedData,BGN_DATE == 1993)$EVTYPE)
 
 
-# Fatalities
-data$FATALITIES
-table(data$FATALITIES)
+# i will split the data (from 1960 t0 1992 - and form 1993 to 2011)
 
-# 
-str(data$PROPDMG)
+SelectedData6092 <- filter(SelectedData, BGN_DATE <= 1992)
+SelectedData9311 <- filter(SelectedData, BGN_DATE > 1992)
+
+# As we can see, from 1960 to 1992 there are only 3 EVTYPE
+
+unique(SelectedData6092$EVTYPE)  #is it useful for the analysis? -- NOt!!!
+
+# As we can see, from 1960 to 1992 there are  many EVTYPE
+unique(SelectedData9311$EVTYPE)
+
+sort(table(SelectedData9311$EVTYPE), decreasing = TRUE)[1:100]
+
+# With the dataset from 1960 to 1992 I explore the events that caused at least 1 
+# death or Injury
+
+SelectedDataF1 <- SelectedData9311 %>% filter(INJURIES > 0 | FATALITIES > 0) 
+
+unique(SelectedDataF1$EVTYPE)
+
